a68511733c1418660640a2950f26f19105edeea1
Apple <Apple@Melpnp-2.local>
2021-03-26 15:39:45 +0700

    second

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs')
+const port = process.env.PORT || 3000
 app.get('/',(req,res)=>{
     const absolutePathToHtmlFile = path.resolve(__dirname, '../dist/index.html');
     const contentOfHtmlFile = fs.readFileSync(absolutePathToHtmlFile, 'utf-8');
@@ -12,6 +13,6 @@ app.use('/static', express.static(
     path.resolve(__dirname, '../dist')
 ));
 
-app.listen(3000,()=>{
-    console.log("server connect http://localhost:"+3000)
+app.listen(port,()=>{
+    console.log("server connect ");
 })
